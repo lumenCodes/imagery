2a0e48b4d95ae1ea4f5987d49b4bc42963b1ad71
cld <lumenluwiz@gmail.com>
2021-04-17 09:54:53 +0100

    Implemented CRUD for image route

--- controllers/image.controller.js
+++ controllers/image.controller.js
@@ -2,26 +2,44 @@ const {Image} = require ('../models/image') //model and schema
 
 
 class ImageController {
-async create(req, res){
-   try {
-    const image = new Image(req.body);
+    async create(req, res){
+        try {
+            const image = new Image(req.body);
 
-    await image.save()
-    res.status(201).send({
-        success: true,
-        message: 'Image created',
-        data: image
-    });
-   } catch (error) {
-       res.send(error)
-   }
+            await image.save()
+            res.status(201).send({
+                success: true,
+                message: 'Image created',
+                data: image
+            });
+        } catch (error) {
+            res.send(error)
+        }
 
-};
+    };
 
-async getAll(req, res){
-    const images = await Image.find()
-res.status(200).send({message: 'this is all the images', data: images});    
-}
+    async getAll(req, res){
+        const images = await Image.find()
+        res.status(200).send({message: 'this is all the images', data: images});    
+    };
+
+    async update(req, res){
+        const image = await Image.findByIdAndUpdate()
+        res.status(200).send({
+            success: true,
+            message: 'Image detail updated succesfully',
+            data: image
+        })
+    };
+
+    async delete(req, res){
+        const image = await Image.findByIdAndDelete()
+        res.status(201).send({
+            success: true,
+            message: 'Image deleted succesfully',
+            data: image
+        })
+    };
 };
 
 
--- routes/image.routes.js
+++ routes/image.routes.js
@@ -7,5 +7,11 @@ const imageController = require('../controllers/image.controller')
 router.post('/', imageController.create)
 
 router.get('/', imageController.getAll)
- 
+
+router.delete('/', imageController.delete)
+
+router.patch('/', imageController.update)
+
+
+
 module.exports = router;
